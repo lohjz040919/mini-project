569502211c67281c273f65f307f95995ac0be06a
lohjz040919 <lohjz040919@student.usm.my>
2025-01-07 19:40:11 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//*Mini Project G24*//
+//*Project Title: Electronic Components Database for PPKEE Stockist*//
+//Group members: Loh Jian Zhe 23302169 | Lee Yong Keat | Khairul Harery bin Awang | Liew Wen Hao | 
+// Muhammad Iman bin Ab Aziz | Muhammad Ali Fansuri bin Azmi | Tan Jui Han //
+
+#include <iostream>
+#include "login_page.hpp"
+#include "borrow_system.hpp"
+#include "return_system.hpp"
+#include "edit_components.hpp"
+#include "check_record.hpp"
+#include "purchase_request.hpp"
+
+int main(){
+    return 0;
+
+}
